7f1e9d2a9f461bfacd605ea6bd6ae102642706f9
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-03 12:36:44 +0530

    Added:GenericUC.java, morethan 3 parameter

--- Generic.java
+++ Generic.java
@@ -1,35 +1,29 @@
 package om.practice.generic;
 
-public class RefactorUC1 {
-	
-	public static void main(String[] args) {
-		Integer[] a = {1, 2, 3}; 
-		String[] s = {"Apple", "Peach", "Banana"};
-		Float[] f = {(float) 1.20, (float) 5.40, (float) 2.02}; 
-		
+public class GenericUC4 
+{
 
-		toPrint(a);
-		toPrint(s);
-		toPrint(f);
-		
-		System.out.println("maximum Value of Integer");
-		System.out.println(max(a));
-		
-		System.out.println("Maximum  value of String is:");
-		System.out.println(max(s));
-	
-		System.out.println("Maximum Float value is:");
-		System.out.println(max(f));
-		
+	public static void main(String[] args){
+
+		System.out.println(max(4, 8, 6));
+		System.out.println(max("Apples", "Peach", "Banana"));
+		System.out.println(max(1.4, 2.5, 6.5));
+		System.out.println(max('h','j','d'));
 	}
-	public static <E extends Comparable<E>> E max(E... elements) {
-		E max = elements[0];
-		for (E element : elements) {
-			if (element.compareTo(max) > 0) {
-				max = element;
-			}
+
+	public static <E extends Comparable<E>> E max(E a, E b,E c) {
+		E max =a;
+		if (b.compareTo(max) >0)
+			max = b;
+		if (c.compareTo(max)>0)
+			max= c;
+
+		return max;	
+	}
+	private static <E> void toPrint(E[] a) {
+
+		for (E i:a) {
+			System.out.println(i);  
 		}
-		return max;
 	}
-
-
+}
