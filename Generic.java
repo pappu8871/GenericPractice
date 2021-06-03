54afe45630672a9102755d66243531eacb77f0d8
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-03 12:17:15 +0530

    Added:GenericUC1, max Integer

--- Generic.java
+++ Generic.java
@@ -1,10 +1,28 @@
+
 package om.practice.generic;
 
-public class WelcomeGeneric {
-
+public class GenericUC1 {
 	public static void main(String[] args) {
-	System.out.println("Welcome To Generic");
+
+		Integer[] a = {1, 2, 3}; 
+		toPrint(a);
+		System.out.println("print maximum Value");
+		System.out.println(max(a));
+
 	}
+	public static <E extends Comparable<E>> E max(E... elements) {
+		E max = elements[0];
+		for (E element : elements) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
+		}
+		return max;
+	}
+	private static <E> void toPrint(E[] a) {
 
+		for (E i:a) {
+			System.out.println(i);  
+		}
+	}
 }
-
