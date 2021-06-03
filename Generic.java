236fbb51a9e6f036fd91ef156d8d56bd810b6336
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-03 12:32:53 +0530

    Added:RefactorUC1

--- Generic.java
+++ Generic.java
@@ -1,29 +1,35 @@
 package om.practice.generic;
 
-public class GenericUC3 {
+public class RefactorUC1 {
+	
+	public static void main(String[] args) {
+		Integer[] a = {1, 2, 3}; 
+		String[] s = {"Apple", "Peach", "Banana"};
+		Float[] f = {(float) 1.20, (float) 5.40, (float) 2.02}; 
+		
 
-public static void main(String[] args) {
-
-	String[] s = {"Apple", "Peach", "Banana"}; 
-	toPrint(s);
-	System.out.println("Maximum String value is:");
-	System.out.println(max(s));
-
-}
-public static <E extends Comparable<E>> E max(E... elements) {
-	E max = elements[0];
-	for (E element : elements) {
-		if (element.compareTo(max) > 0) {
-			max = element;
+		toPrint(a);
+		toPrint(s);
+		toPrint(f);
+		
+		System.out.println("maximum Value of Integer");
+		System.out.println(max(a));
+		
+		System.out.println("Maximum  value of String is:");
+		System.out.println(max(s));
+	
+		System.out.println("Maximum Float value is:");
+		System.out.println(max(f));
+		
+	}
+	public static <E extends Comparable<E>> E max(E... elements) {
+		E max = elements[0];
+		for (E element : elements) {
+			if (element.compareTo(max) > 0) {
+				max = element;
+			}
 		}
+		return max;
 	}
-	return max;
-}
 
-private static <E> void toPrint(E[] a) {
 
-	for (E i:a) {
-		System.out.println(i);  
-	}
-}
-}
